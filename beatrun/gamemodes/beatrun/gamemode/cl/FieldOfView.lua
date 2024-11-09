fbcd70d350efd318f5a014372c36fbee204cd30b
LostTrackpad <lamhung12700@gmail.com>
2024-11-09 16:47:50 +0700

    More flexible FOV remap

--- beatrun/gamemodes/beatrun/gamemode/cl/FieldOfView.lua
+++ beatrun/gamemodes/beatrun/gamemode/cl/FieldOfView.lua
@@ -4,7 +4,7 @@ local fovtest = CreateClientConVar("beatrun_fovtesting", 0, true, false, "", 0, 
 function ModifyFOV(ply, opos, oang, ofov, ...)
     local view = hook.Run("CalcView", ply, opos, oang, ofov, ...)
     
-    view.fov = math.Remap(view.fov or ofov, 20, GetConVar("fov_desired"):GetInt(), 20, GetConVar("beatrun_fov"):GetInt())
+    view.fov = math.Remap(view.fov or ofov, 0, GetConVar("fov_desired"):GetFloat(), 0, GetConVar("beatrun_fov"):GetFloat())
 
     return view
 end
